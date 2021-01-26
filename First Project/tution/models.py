2f8a87c7248036aa64d4c2156dab15d9a4c75eba
MasumTech <masumrezadiu@gmail.com>
2021-01-26 15:00:48 +0600

    Post model created

--- First Project/tution/models.py
+++ First Project/tution/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.timezone import now
 # Create your models here.
 
 class Contact(models.Model):
@@ -10,4 +10,21 @@ class Contact(models.Model):
     def __str__(self):
         return self.name
     
+class Post(models.Model):
+    CATEGORY =(
+        ('Teacher' , 'Teacher'),
+        ('Student' , 'Student'),
+    )
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100, default=title)
+    email = models.EmailField(max_length=100)
+    salary = models.IntegerField()
+    details = models.TextField(max_length=600)
+    available = models.BooleanField()
+    category = models.CharField(max_length=100, choices=CATEGORY)
+    created_at = models.DateTimeField(default=now)
 
+    def __str__(self):
+        return self.title 
+    
