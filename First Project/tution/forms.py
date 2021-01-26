52a1d7c4e6efb9238cb22d4506fa1f2f2e483e40
MasumTech <masumrezadiu@gmail.com>
2021-01-26 12:09:23 +0600

    FormModel updated

--- First Project/tution/forms.py
+++ First Project/tution/forms.py
@@ -1,7 +1,7 @@
 from django import forms
+from .models import Contact
 
-
-class  ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, label="Your Name")
-    phone = forms.CharField(max_length=100, label="Your Phone")
-    message = forms.CharField(max_length=100, label="Your Message")
+class  ContactForm(forms.ModelForm):
+      class Meta:
+          model = Contact
+          fields = '__all__'
--- First Project/tution/views.py
+++ First Project/tution/views.py
@@ -8,12 +8,8 @@ def contact(request):
     if request.method=='POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            name = form.cleaned_data['name']
-            phone = form.cleaned_data['phone']
-            message = form.cleaned_data['message'] 
-            
-            obj = Contact(name=name, phone=phone, message=message)
-            obj.save()
+           form.save() 
+           
     else:
         form = ContactForm()   
     return render(request, 'contact.html', {'form':form})    
